ed51cb14440b276a444fc6a07e69f02b3ceceb99
Oleksandr <oleksandr.karnaushenko@gmail.com>
2024-07-14 12:07:04 +0200

    add CountryInfo

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,24 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryInfo, Heading, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  const [country, setCoutry] = useState(null);
+  useEffect(() => {
+    async function coutryInfo() {
+      const info = await fetchCountry(countryId);
+      setCoutry(info);
+    }
+
+    coutryInfo();
+  }, []);
   return (
     <Section>
       <Container>
         <Heading title="SearchCountry" bottom />
+        {country && <CountryInfo {...country} />}
       </Container>
     </Section>
   );
